a70485ee4bf44d581d1e9590a9a54304b38a225e
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2016-11-16 16:06:08 -0200

    Changes at security login at UI service

--- ui-service/src/main/app/scripts/controllers/home.controller.js
+++ ui-service/src/main/app/scripts/controllers/home.controller.js
@@ -2,19 +2,23 @@ define(['app'], function () {
 
     'use strict';
 
-    return ['$scope','$rootScope','BaseController', function ($scope, $rootScope, BaseController) {
+    return ['$scope','$rootScope', '$http', '$log', 'toastr', 'BaseController', function ($scope, $rootScope, $http, $log, toastr, BaseController) {
 
             angular.extend($scope, BaseController);
-
-            $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-                console.log('state Change Success');
-            });
 
             /**
              * Listener when the view is loaded
              */
             $scope.$on('$viewContentLoaded', function() {
-                console.log('view Content Loaded...');
+                console.log('view Content Loaded');
+                $http.get("http://localhost:9999/uaa/user")
+                    .success( function (data, status, headers, config ) {
+                        $scope.user = data;
+                        $scope.user.stringify = JSON.stringify(data, undefined, 2);
+                    })
+                    .error( function (data, status, headers, config ){
+                        $log.error( data );
+                    });
             });
 
         }];
