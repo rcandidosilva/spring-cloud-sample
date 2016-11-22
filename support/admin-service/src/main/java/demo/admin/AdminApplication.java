725c2585b5e5d495d383b76c115faa4b03a93685
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2016-11-22 18:20:06 -0200

    Added Admin service configuration

---
+++ support/admin-service/src/main/java/demo/admin/AdminApplication.java
@@ -0,0 +1,28 @@
+package demo.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdminApplication.class);
+	}
+
+}
